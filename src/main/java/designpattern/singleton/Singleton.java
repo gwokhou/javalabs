d579b40b829bc95ada78b939f59ff00ccc0e4b3f
ghz54 <akidezhang@outlook.com>
2019-08-29 20:54:46 +0800

    design pattern - singleton

---
+++ src/main/java/designpattern/singleton/Singleton.java
@@ -0,0 +1,16 @@
+package designpattern.singleton;
+
+/**
+ * @author Guo-hao Zhang
+ */
+public class Singleton {
+    private static Singleton singleton = new Singleton();
+
+    private Singleton() {
+        System.out.println("生成了一个实例");
+    }
+
+    public static Singleton getInstance() {
+        return singleton;
+    }
+}
